cca9a7f7a280a5ceae2eadf0992bc3d4aa386e48
NaveenKumar <58531161+thephotogenicbug@users.noreply.github.com>
2020-04-03 00:08:48 +0530

    bump Atom Version to 2.6.1+69

--- config/branding.mk
+++ config/branding.mk
@@ -1,6 +1,6 @@
 # Versioning System
-ATOM_BASE_VERSION = 2.6
-ATOM_CODENAME := Quantizer
+ATOM_BASE_VERSION = 2.6.1+69
+ATOM_CODENAME := Go Corona
 
 # Set all versions
 CUSTOM_BUILD_TYPE ?= UNOFFICIAL

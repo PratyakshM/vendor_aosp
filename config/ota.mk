Evolution-X-AOSP/vendor_evolution

ota: system --> TARGET_COPY_OUT_SYSTEM

config/ota.mk
@@ -10,6 +10,6 @@ PRODUCT_PACKAGES += \
     Updates
 
 PRODUCT_COPY_FILES += \
-    vendor/aosp/config/permissions/org.evolution.ota.xml:system/etc/permissions/org.evolution.ota.xml
+    vendor/aosp/config/permissions/org.evolution.ota.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.evolution.ota.xml
 
 endif

PratyakshM/vendor_aosp

config/ota.mk
@@ -9,6 +9,6 @@ PRODUCT_PACKAGES += \
     Updates
 
 PRODUCT_COPY_FILES += \
-    vendor/aosp/config/permissions/org.atom.ota.xml:system/etc/permissions/org.atom.ota.xml
+    vendor/aosp/config/permissions/org.atom.ota.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.atom.ota.xml
 
 endif
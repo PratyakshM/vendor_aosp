Evolution-X-AOSP/vendor_evolution

ota: Use CUSTOM_PLATFORM_VERSION for OTA versioning
Change-Id: Id7bbc6bc74f1d3cf747f0279fb01a2b6adc5f22e

config/branding.mk
@@ -25,6 +25,7 @@ ROM_FINGERPRINT := EvolutionX/$(EVO_CODENAME)/$(CUSTOM_PLATFORM_VERSION)/$(TARGE
 
 CUSTOM_PROPERTIES := \
     org.evolution.version=$(EVO_VERSION) \
+    org.evolution.version.prop=$(CUSTOM_PLATFORM_VERSION) \
     org.evolution.version.display=$(CUSTOM_VERSION) \
     org.evolution.build_version=$(EVO_BASE_VERSION) \
     org.evolution.build_date=$(CUSTOM_BUILD_DATE) \

PratyakshM/vendor_aosp

config/branding.mk
@@ -25,6 +25,7 @@ CUSTOM_VERSION_PROP := 10
 CUSTOM_PROPERTIES := \
     org.atom.version=$(CUSTOM_VERSION_PROP) \
     org.atom.version=$(ATOM_VERSION) \
+    org.atom.version.prop=$(CUSTOM_PLATFORM_VERSION) \
     org.atom.version.display=$(CUSTOM_VERSION) \
     org.atom.build_version=$(ATOM_BASE_VERSION) \
     org.atom.version.display=$(ATOM_VERSION) \